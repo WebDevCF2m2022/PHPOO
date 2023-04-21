40c7dd05563e949f9378bd04579829d08dd28f0d
mikhawa <michaeljpitz@gmail.com>
2023-04-21 10:47:56 +0200

    attributs, constantes - méthodes

--- 01/MaPremiereClass.php
+++ 01/MaPremiereClass.php
@@ -3,6 +3,14 @@
 
 class MaPremiereClass{
     // Attributs -> variables avec visibilités
-    // Constantes -> constantes (avec lecture seule, etc...)
+    public $premierAttribut;
+    public $deuxiemeAttribut;
+    // Constantes -> constantes (avec lecture seule, etc...) par défaut publique
+    public const PREMIERE_CONSTANTE = "Une valeur obligatoire, lisible mais non modifiable";
+    public const DEUXIEME_CONSTANTE = 5;
+
     // Méthodes -> fonctions avec visibilités
+    public  function helloWorld(){
+        return "Hello World !";
+    }
 }
--- 01/index.php
+++ 01/index.php
@@ -20,11 +20,54 @@ require "MaPremiereClass.php";
     // instanciations de 2 classes
     $class1 = new MaPremiereClass();
     $class2 = new MaPremiereClass();
-    // création d'un lien symbolique vers la 2eme classe (pas un clône de la classe)
+    // création d'un lien symbolique vers la 2eme classe (pas un clône de la classe) = ALIAS
     $class3 = $class2;
 
     var_dump($class1,$class2,$class3);
     ?>
+    <h2>Attributs public</h2>
+    <h4>Les propriétés visibles (public) des attributs permmettent de les lire et/ou écricre en dehors de la classe !</h4>
+    <p>Affichage / modification Attributs publics</p>
+    <p>$instance->nomDeLattribut = 5; // modification</p>
+    <p>echo $instance->nomDeLattribut; // affichage</p>
+    <?php
+    // attribution d'une valeur a un attribut publique:
+    $class1->premierAttribut = "coucou";
+    // affichage d'un attribut publique
+    echo $class1->premierAttribut;
 
+    $class2->deuxiemeAttribut = 25;
+
+    // création d'un attribut à la volée, dangereux et inutile, on pourra bloquer cette fonctionnalité !
+    $class2->troisiemeAttribut = 50;
+
+    ?>
+<h2>Les constantes</h2>
+<h4>Par défauts public, ne peuvent être modifiées, doivent être remplies à la création</h4>
+<p>echo ClassName::CONSTANTE</p>
+<p>Bonne pratique, depuis la class et non pas une instanciation</p>
+    <?php
+    // mauvaise pratique, affichage depuis l'instance
+    echo $class2::PREMIERE_CONSTANTE;
+    // bonne pratique, depuis la classe
+    echo MaPremiereClass::PREMIERE_CONSTANTE;
+
+    // Erreur fatale, on ne peut modifier une constante
+    //MaPremiereClass::DEUXIEME_CONSTANTE = "coucou";
+
+    
+    ?>
+<h2>Les Méthodes</h2>
+<h4>Les méthodes publiques sont des actions qu'on peut utiliser en dehors de la class ou des instances de classe.</h4>
+<p>echo $class1->helloWorld();</p>
+<?php
+echo $class1->helloWorld();
+
+// erreur car non static
+//echo MaPremiereClass::helloWorld();
+
+
+var_dump($class1,$class2,$class3);
+?>
 </body>
 </html>
