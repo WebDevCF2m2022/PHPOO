63c819720f452b26da816ad27126742c0bb1496f
mikhawa <michaeljpitz@gmail.com>
2023-05-25 12:17:21 +0200

    autoload

--- 07-mapping-manager/public/index.php
+++ 07-mapping-manager/public/index.php
@@ -1,4 +1,18 @@
 <?php
+// Lancement de session
 session_start();
+
+// constantes de connexion (et autres constantes globales)
+require_once "../configuration/config.php";
+
+// Autoload de classes (qui portent le même nom que le fichier) ! Gestion des namespaces
+spl_autoload_register(function ($className) {
+    // par exemple si on est dans le dossier public
+    $file = '../model/' . str_replace('\\', '/', $className) . '.php';
+    // si le fichier existe, on le require_once
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
 
 
