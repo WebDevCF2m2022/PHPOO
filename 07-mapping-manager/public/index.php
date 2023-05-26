9b27d9bee793b99c84340f338a51c8581e823f7c
mikhawa <michaeljpitz@gmail.com>
2023-05-26 10:00:53 +0200

    comment index

--- 07-mapping-manager/public/index.php
+++ 07-mapping-manager/public/index.php
@@ -7,9 +7,9 @@ require_once "../configuration/config.php";
 
 // Autoload de classes (qui portent le même nom que le fichier) ! Gestion des namespaces
 spl_autoload_register(function ($className) {
-    // par exemple si on est dans le dossier public
+    // par exemple si on est dans le dossier public, le str_replace va remplacer les \ des namespaces par des / et on aura par exemple ../model/Entity/Entity.php
     $file = '../model/' . str_replace('\\', '/', $className) . '.php';
-    // si le fichier existe, on le require_once
+    // si le fichier existe, on le charge
     if (file_exists($file)) {
         require_once $file;
     }
