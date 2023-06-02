2f198266b64e80540ef30c68b5b9baa802f76421
mikhawa <michaeljpitz@gmail.com>
2023-06-02 11:12:45 +0200

    autoload ok

---
+++ 07-abstract-trait-implements/public/index.php
@@ -0,0 +1,31 @@
+<?php
+// démarre la session
+session_start();
+
+// charge le fichier de configuration
+require_once '../configuration/config.php';
+
+// autoload de nos classes depuis la racine PATH_ROOT en suivant l'arborescence
+// des namespaces de nos classes
+spl_autoload_register(
+    function ($className) {
+        $className = str_replace('\\', '/', $className);
+        require_once PATH_ROOT. '/' . $className . '.php';
+    });
+
+// connexion à la base de données en PDO
+try {
+    $pdo = new PDO(
+        DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET . ';port=' . DB_PORT,
+        DB_USER,
+        DB_PWD,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+} catch (PDOException $e) {
+    echo 'Erreur de connexion : ' . $e->getMessage();
+}
+
+
